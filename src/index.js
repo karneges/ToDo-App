cd09235c99db16fa0eecfb65c291f3f9e48e29d7
Nikita <karneges2012@gmail.com>
2020-01-04 19:35:42 +0200

    add esLint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+const TodoList = () => {
+    return (
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awessom App</li>
+        </ul>
+    );
+};
+
+const AppHeader = () => {
+    return (
+        <h1> My Todo List</h1>
+    );
+};
+
+const SearchPanel = () => {
+    return (
+        <input placeholder="search" />
+    );
+};
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList />
+        </div>
+    );
+};
+
+ReactDOM.render(<App/>,
+    document.getElementById('root'));
+
 
