2c9b4918ca67251644f0d5b6da37c767e5f33085
Nikita <karneges2012@gmail.com>
2020-01-05 12:28:40 +0200

    add CSS

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,25 +1,23 @@
-/* eslint-disable react/prop-types */
-
 import React from 'react';
+
 import TodoListItem from './todo-list-item';
+import './todo-list.css';
+
 const TodoList = ({ todos }) => {
+
+  const elements = todos.map((item) => {
+    const { id, ...itemProps } = item;
+
+    return (
+      <li key={id} className="list-group-item">
+        <TodoListItem {...itemProps } />
+      </li>
+    );
+  });
+
   return (
-    <ul>
-      <li>
-        <TodoListItem
-          label={todos[0].label}
-          important={todos[0].important} />
-      </li>
-      <li>
-        <TodoListItem
-          label={todos[1].label}
-          important={todos[1].important} />
-      </li>
-      <li>
-        <TodoListItem
-          label={todos[2].label}
-          important={todos[2].important} />
-      </li>
+    <ul className="list-group todo-list">
+      { elements }
     </ul>
   );
 };
